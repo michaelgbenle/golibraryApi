82962ff72d830098e56c3b873486e46f196658a6
Michael <michaelgbenle@gmail.com>
2022-07-02 15:31:37 +0100

    go library

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ type book struct {
 }
 
 var books = []book{
-	{ID: "1"},
-	{},
-	{},
+	{ID: "1", Title: "Rules of life", Author: "Jordan Peterson", Quantity: 12},
+	{ID: "2", Title: "Rules of love", Author: "Kate Peterson", Quantity: 10},
+	{ID: "3", Title: "Rules of wealth", Author: "Jordan Peterson", Quantity: 12},
+	{ID: "4", Title: "Rules of health", Author: "Kate Peterson", Quantity: 11},
 }
