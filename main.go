29e253e1cbe986681148e532d77a755402ee91d1
Michael <michaelgbenle@gmail.com>
2022-07-03 10:52:05 +0100

    go library

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import (
 	"github.com/joho/godotenv"
 	"golibraryApi/router"
 	"log"
+	"os"
 )
 
-func LoadEnv(path string) {
-	err := godotenv.Load(path)
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
-}
+//func LoadEnv(path string) {
+//	err := godotenv.Load(path)
+//	if err != nil {
+//		log.Fatalln(err)
+//		return
+//	}
+//}
 func main() {
+	everr := godotenv.Load(".env")
+	if everr != nil {
+		log.Fatal(everr)
+	}
+	port := os.Getenv("PORT")
 	library := router.SetupRouter()
-	err := library.Run(":8080")
+	err := library.Run(":" + port)
 	if err != nil {
 		log.Fatal(err)
 	}
