327918e6221c4bbe7d4ef22a96302589cbd3f64b
Michael <michaelgbenle@gmail.com>
2022-07-03 10:41:40 +0100

    go library

--- database/database.go
+++ database/database.go
@@ -3,13 +3,24 @@ package database
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 )
 
 type PostgresDb struct {
 	DB *gorm.DB
 }
 
-func SetupDb() {
+//Export an instance of database
+func NewPostgresDb() *PostgresDb {
+	return &PostgresDb{}
+}
+func (pdb *PostgresDb) SetupDb() {
 	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Africa/Lagos"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	pdb.DB = db
+
+	err
 }
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"golibraryApi/router"
 	"log"
 )
 
+func LoadEnv(path string) {
+	err := godotenv.Load(path)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+}
 func main() {
 	library := router.SetupRouter()
 	err := library.Run(":8080")
