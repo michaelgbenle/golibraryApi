232dedb1f21241c1b673ca4b011bf4ab8ce2143a
Michael <michaelgbenle@gmail.com>
2022-07-03 16:45:39 +0100

    go library

--- database/database.go
+++ database/database.go
@@ -65,7 +65,7 @@ func (pdb *PostgresDb) Checkout(id, copies string) (*models.Book, error) {
 func (pdb *PostgresDb) Checkin(id, copies string) (*models.Book, error) {
 	var book *models.Book
 	intCopies, _ := strconv.Atoi(copies)
-	newQuantity := book.Quantity - intCopies
+	newQuantity := book.Quantity + intCopies
 	if err := pdb.DB.Model(book).Where("id = ?", id).Update(strconv.Itoa(book.Quantity), newQuantity).Error; err != nil {
 		return nil, err
 	}
