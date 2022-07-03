de2a456511cee48455b5fe954573c4b8a2ad2cfa
Michael <michaelgbenle@gmail.com>
2022-07-03 10:07:37 +0100

    go library

--- router/router.go
+++ router/router.go
@@ -8,7 +8,7 @@ import (
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/books", handler.getBooks)
-	router.POST("/addbook", addBook)
+	router.POST("/addbook", handler.addBook)
 	router.GET("/getbook/:id", getBookById)
 	router.PATCH("/checkout", checkOutBook)
 	router.PATCH("/return", returnBook)
