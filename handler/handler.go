0f71f44b1ad333a6019eb84f8e9ac3fe13f742e0
Michael <michaelgbenle@gmail.com>
2022-07-03 09:49:16 +0100

    go library

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,69 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func bookById(id string) (*book, error) {
+	for i, v := range books {
+		if v.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": books})
+}
+func getBookById(c *gin.Context) {
+	id := c.Param("id")
+	sBook, err := bookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"message": sBook})
+}
+func addBook(c *gin.Context) {
+	var newBook book
+	err := c.ShouldBindJSON(&newBook)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "unable to bind json"})
+		c.Abort()
+		return
+	}
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": newBook})
+}
+func checkOutBook(c *gin.Context) {
+	id := c.Query("id")
+	sBook, err := bookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not found"})
+		return
+	}
+	if sBook.Quantity <= 0 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not available"})
+		return
+	}
+	sBook.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"message": "book successfully returned",
+		"updated": sBook,
+	})
+}
+
+func returnBook(c *gin.Context) {
+	id := c.Query("id")
+	sBook, err := bookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not found"})
+		return
+	}
+	sBook.Quantity += 1
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"message": "book successfully returned",
+		"updated": sBook,
+	})
+}
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"errors"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type book struct {
@@ -18,69 +16,6 @@ var books = []book{
 	{ID: "2", Title: "Rules of love", Author: "Kate Peterson", Quantity: 10},
 	{ID: "3", Title: "Rules of wealth", Author: "Jordan Peterson", Quantity: 12},
 	{ID: "4", Title: "Rules of health", Author: "Kate Peterson", Quantity: 11},
-}
-
-func bookById(id string) (*book, error) {
-	for i, v := range books {
-		if v.ID == id {
-			return &books[i], nil
-		}
-	}
-	return nil, errors.New("book not found")
-}
-func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{"message": books})
-}
-func getBookById(c *gin.Context) {
-	id := c.Param("id")
-	sBook, err := bookById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not found"})
-		return
-	}
-	c.IndentedJSON(http.StatusOK, gin.H{"message": sBook})
-}
-func addBook(c *gin.Context) {
-	var newBook book
-	err := c.ShouldBindJSON(&newBook)
-	if err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "unable to bind json"})
-		c.Abort()
-		return
-	}
-	books = append(books, newBook)
-	c.IndentedJSON(http.StatusCreated, gin.H{"message": newBook})
-}
-func checkOutBook(c *gin.Context) {
-	id := c.Query("id")
-	sBook, err := bookById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not found"})
-		return
-	}
-	if sBook.Quantity <= 0 {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not available"})
-		return
-	}
-	sBook.Quantity -= 1
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"message": "book successfully returned",
-		"updated": sBook,
-	})
-}
-
-func returnBook(c *gin.Context) {
-	id := c.Query("id")
-	sBook, err := bookById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "book not found"})
-		return
-	}
-	sBook.Quantity += 1
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"message": "book successfully returned",
-		"updated": sBook,
-	})
 }
 
 func main() {
