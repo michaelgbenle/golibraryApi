aed12fa8bf097b72404639189f38ce6c6a992b77
Michael <michaelgbenle@gmail.com>
2022-07-03 10:05:45 +0100

    go library

--- main.go
+++ main.go
@@ -5,20 +5,6 @@ import (
 	"log"
 )
 
-type book struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Author   string `json:"author"`
-	Quantity int    `json:"quantity"`
-}
-
-var books = []book{
-	{ID: "1", Title: "Rules of life", Author: "Jordan Peterson", Quantity: 12},
-	{ID: "2", Title: "Rules of love", Author: "Kate Peterson", Quantity: 10},
-	{ID: "3", Title: "Rules of wealth", Author: "Jordan Peterson", Quantity: 12},
-	{ID: "4", Title: "Rules of health", Author: "Kate Peterson", Quantity: 11},
-}
-
 func main() {
 	library := router.SetupRouter()
 	err := library.Run(":8080")
--- models/book.go
+++ models/book.go
@@ -1,1 +1,15 @@
 package models
+
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "Rules of life", Author: "Jordan Peterson", Quantity: 12},
+	{ID: "2", Title: "Rules of love", Author: "Kate Peterson", Quantity: 10},
+	{ID: "3", Title: "Rules of wealth", Author: "Jordan Peterson", Quantity: 12},
+	{ID: "4", Title: "Rules of health", Author: "Kate Peterson", Quantity: 11},
+}
