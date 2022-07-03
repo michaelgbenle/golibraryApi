a73f7b35ae65dec809fb82bc4ba43412cd2fbbcc
Michael <michaelgbenle@gmail.com>
2022-07-03 10:23:46 +0100

    go library

--- database/database.go
+++ database/database.go
@@ -1,5 +1,11 @@
 package database
 
-func SetupDb() {
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
+func SetupDb() {
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
