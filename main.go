eefc710dbbcbe75f96d34301c01ee668c8c13f39
Michael <michaelgbenle@gmail.com>
2022-07-03 10:54:15 +0100

    go library

--- main.go
+++ main.go
@@ -7,19 +7,13 @@ import (
 	"os"
 )
 
-//func LoadEnv(path string) {
-//	err := godotenv.Load(path)
-//	if err != nil {
-//		log.Fatalln(err)
-//		return
-//	}
-//}
 func main() {
 	everr := godotenv.Load(".env")
 	if everr != nil {
 		log.Fatal(everr)
 	}
 	port := os.Getenv("PORT")
+
 	library := router.SetupRouter()
 	err := library.Run(":" + port)
 	if err != nil {
