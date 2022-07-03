0feee85fafca241da419234d6f57b7cd9a2109d1
Michael <michaelgbenle@gmail.com>
2022-07-03 10:25:34 +0100

    go library

--- database/database.go
+++ database/database.go
@@ -5,6 +5,10 @@ import (
 	"gorm.io/gorm"
 )
 
+type PostgresDb struct {
+	DB *gorm.DB
+}
+
 func SetupDb() {
 	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Africa/Lagos"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
