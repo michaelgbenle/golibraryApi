d85b6c2b74cbe79058862443e2ac944d5780ab77
Michael <michaelgbenle@gmail.com>
2022-07-03 10:07:21 +0100

    go library

--- router/router.go
+++ router/router.go
@@ -1,17 +1,15 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"golibraryApi/handler"
+)
 
-router.GET("/books", getBooks)
-router.POST("/addbook", addBook)
-router.GET("/getbook/:id", getBookById)
-router.PATCH("/checkout", checkOutBook)
-router.PATCH("/return", returnBook)
-err := router.Run("localhost:8080")
-if err != nil {
-return
-}
-
-func SetupRouter() *gin.Engine{
+func SetupRouter() *gin.Engine {
 	router := gin.Default()
+	router.GET("/books", handler.getBooks)
+	router.POST("/addbook", addBook)
+	router.GET("/getbook/:id", getBookById)
+	router.PATCH("/checkout", checkOutBook)
+	router.PATCH("/return", returnBook)
 }
